66a50f3bf886ba467b0cc322097dddadea0cf067
FeiAline <yufei.flyingfish@gmail.com>
2013-03-13 22:44:12 +0800

    加了subscribe 的一些东西，archive 作者的问题Fixed

--- sub/subscribe_back.php
+++ sub/subscribe_back.php
@@ -4,7 +4,7 @@
     ini_set('display_errors', true);
     define('DATABASE_HOST', 'localhost');
     define('DATABASE_USER', 'root');
-    define('DATABASE_PASS', 'root');
+    define('DATABASE_PASS', 'postile123');
     define('DATABASE_NAME', 'typecho');
 
     @mysql_connect( DATABASE_HOST, DATABASE_USER, DATABASE_PASS) or die('Unable to connect to database server.');
